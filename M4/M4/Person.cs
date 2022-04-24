44353a545b14b8419ad88fda28180fb44403af0f
Niedritis <niedritis@inbox.lv>
2022-04-24 21:26:16 +0300

    hhh

---
+++ M4/M4/Person.cs
@@ -0,0 +1,19 @@
+﻿namespace M4
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Hobby { get; set; }
+        public bool Gender { get; set; }
+
+        public int GetAge()
+        {
+            return DateTime.Now.Year - BirthDate.Year;
+        }
+    }
+
+
+}
+
